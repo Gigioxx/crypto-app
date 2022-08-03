e6950b5febe39c0fc2a1a69e17a907a79453cc86
Gigiox <guillermo.casanova.b@gmail.com>
2022-08-03 19:40:17 -0400

    Added routing and styles to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { styled } from '@mui/material/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage';
+
+const StyledRoot = styled('div')({
+  backgroundColor: '#14161a',
+  color: 'white',
+  minHeight: '100vh',
+});
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <StyledRoot>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/coin/:id' element={<CoinPage />} />
+        </Routes>
+      </StyledRoot>
+    </Router>
+  );
 }
 
 export default App;
