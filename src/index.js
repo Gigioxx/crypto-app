52a78359dc0fd12326c0b233b80a4e96409f7e03
Gigiox <guillermo.casanova.b@gmail.com>
2022-08-03 22:04:12 -0400

    Added alice-carousel library styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import CryptoContext from './CryptoContext';
 
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
