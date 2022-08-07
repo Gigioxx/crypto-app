6aefc280bb474c8657849ce194c316190a332e66
Gigiox <guillermo.casanova.b@gmail.com>
2022-08-07 02:39:18 -0400

    Added styles to div instead of using styled

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { styled } from '@mui/material/styles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import CoinPage from './Pages/CoinPage';
 import Homepage from './Pages/Homepage';
 
-const StyledRoot = styled('div')({
-  backgroundColor: '#14161a',
-  color: 'white',
-  minHeight: '100vh',
-});
-
 function App() {
   return (
     <Router>
-      <StyledRoot>
+      <div
+        style={{
+          backgroundColor: '#14161a',
+          color: 'white',
+          minHeight: '100vh',
+        }}
+      >
         <Header />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/coin/:id' element={<CoinPage />} />
         </Routes>
-      </StyledRoot>
+      </div>
     </Router>
   );
 }
