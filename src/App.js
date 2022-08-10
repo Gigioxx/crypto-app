1bbdeae09766b3a1f7ee3e13b3998e2478fbde53
Gigiox <guillermo.casanova.b@gmail.com>
2022-08-09 23:14:42 -0400

    Changed names of imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import CoinPage from './Pages/CoinPage';
-import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage/CoinPage';
+import Homepage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
